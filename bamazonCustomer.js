7cf9bcc8a2a047ce1581be8a48a9d02facac82e0
The_Costa <bryan.costa@roadrunner.com>
2019-05-09 21:38:21 -0700

    Trying to get .then statement to work on promt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,10 +14,16 @@ const db = createConnection({
 //     console.log(`IT'S WORKING!!!`)
 // })
 
+let opening = _ => {
+    db.query('SELECT * FROM products')
+    return
+}
+
+opening()
 prompt([
 {
         type: 'list',
-        name: 'intro',
+        name: 'item_id',
         message: 'Welcome to Bamazon! Please select ID of the product that you would like to buy',
         choices: [1,2,3,4,5,6,7,8,9,10]
 },
@@ -26,8 +32,18 @@ prompt([
     name: 'qty',
     message: 'How many items would you like to purchase?'
 }
-]).then(({ intro, qty }) => {
-    console.log(intro)
-    console.log(qty)
-    db.end()
-})
+]).then(({ item_id, qty }) => {
+    db.query(`SELECT * FROM products WHERE ?`, {item_id}, (e, data) => {
+        if (e) throw e
+        console.log(data.stock_quantity)
+        // if (data.stock_quantity <= 0) {
+        // // let {item_id: ID, product_name: Product, price: Price, stock_quantity: Inventory} = data
+        // console.log(data)
+        // // {ID, Product, Price, Inventory}
+        // db.end()
+        // }else{
+        //     console.log("Insufficient inventory")
+        //     db.end()
+        // }
+    })
+}) 
